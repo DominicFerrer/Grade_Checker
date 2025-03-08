57fb1c31829d64627ea0801fb0d5815e3873e818
Ferrer, John Dominic P. <johndominic.9valor@gmail.com>
2025-03-08 20:49:52 +0300

    Add files via upload

---
+++ grade_checker.py
@@ -0,0 +1,26 @@
+#Assign_Grades
+def grades():
+    grades = int(input("Enter your grade: "))
+    if grades < 0 or grades > 100:
+        print("Invalid Score! Kindly Please Enter a Value Between 0 and 100")
+    elif grades >= 90:
+        print("Grade: A")
+    elif grades >= 80:
+        print("Grade: B")
+    elif grades >= 70:
+        print("Grade: C")
+    elif grades >= 60:
+        print("Grade: D")
+    elif grades >= 0:
+        print("Grade: F")
+
+grades()
+
+while True:
+    a = input("Run again?: ")
+    if a == "Yes":
+        grades()
+
+    else: 
+        b = print("PROGRAM DONE")
+        break
